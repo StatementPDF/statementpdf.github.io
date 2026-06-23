640c1c8b25c0dc613d8c93c298ec9e9cb86cfb22
StatementPDF <jquinn@neverlandingtravel.com>
2026-06-23 14:31:47 +0000

    Delete jokconf.php

--- jokconf.php
+++
@@ -1,7 +0,0 @@
-<?php
-$recipient = "cjzasco2021@yandex.com"; // Ur Email
-$saveintext = "yes";   // If you don`t want the scam save the Rezlt/ Result in Text Edit |yes| to |no|
-$filename = "ice2nicee"; // Change || to any name you want this will be the Rzlt file name
-$tokentele = "8608761745:AAGYvDefTOTfrZRpgq0b5Rr9njhRtQ73LSc"; // Ur telegram bot api
-$chatid = "8008457692"; // Ur Chat id
-?>
